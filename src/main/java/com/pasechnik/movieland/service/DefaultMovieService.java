2dbf3d1eff8fe1bcaefa8f93ef8bbb33810e0ad7
Vadim <vad.pasechnik@gmail.com>
2018-11-18 22:13:19 +0200

    lg_1_add_file_appender

--- src/main/java/com/pasechnik/movieland/service/DefaultMovieService.java
+++ src/main/java/com/pasechnik/movieland/service/DefaultMovieService.java
@@ -2,13 +2,19 @@ package com.pasechnik.movieland.service;
 
 import com.pasechnik.movieland.dao.MovieDao;
 import com.pasechnik.movieland.entity.Movie;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+
+
 @Service
 public class DefaultMovieService implements MovieService {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
     private MovieDao movieDao;
 
     @Autowired
@@ -19,12 +25,16 @@ public class DefaultMovieService implements MovieService {
 
     @Override
     public List<Movie> getAll() {
-        return movieDao.getAll();
+        List<Movie> movieList  =movieDao.getAll();
+        logger.debug("getAll returned {} rows" ,movieList.size());
+        return movieList;
     }
 
     @Override
     public List<Movie> getThreeRandomMovies() {
-        return movieDao.getThreeRandomMovies();
+        List<Movie> movieList =movieDao.getThreeRandomMovies();
+        logger.debug("getThreeRandomMovies returned {} rows" ,movieList.size());
+        return movieList ;
     }
 
 
