8403fcc758dc66cc6dcfee0b64e78cdd5bac651e
Vadim <vad.pasechnik@gmail.com>
2018-11-18 18:06:02 +0200

    b-2_get_3_random_movies

---
+++ src/test/java/com/pasechnik/movieland/service/DefaultMovieServiceTest.java
@@ -0,0 +1,106 @@
+package com.pasechnik.movieland.service;
+
+import com.pasechnik.movieland.dao.MovieDao;
+import com.pasechnik.movieland.entity.Movie;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class DefaultMovieServiceTest {
+
+    @Test
+    public void getAll() {
+        MovieDao movieDao = mock(MovieDao.class);
+
+        List<Movie> expectedMovieList = new ArrayList<>();
+
+        Movie movie1 = new Movie();
+        movie1.setId(1);
+        movie1.setNameRussian("Список Шиндлера");
+        movie1.setNameNative("Schindler's List");
+        movie1.setYearOfRelease("1993");
+        movie1.setRating(8.7);
+        movie1.setPrice(150.5);
+        movie1.setPicturePath("https://images-na.ssl-images-amazon.com/images/M/MV5BNDE4OTMxMTctNmRhYy00NWE2LTg3YzItYTk3M2UwOTU5Njg4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1._SX140_CR0,0,140,209_.jpg");
+        expectedMovieList.add(movie1);
+
+        Movie movie2 = new Movie();
+        movie2.setId(2);
+        movie2.setNameRussian("Пролетая над гнездом кукушки");
+        movie2.setNameNative("One Flew Over the Cuckoo''s Nest");
+        movie2.setYearOfRelease("1975");
+        movie2.setRating(8.7);
+        movie2.setPrice(180);
+        movie2.setPicturePath("https://images-na.ssl-images-amazon.com/images/M/MV5BZjA0OWVhOTAtYWQxNi00YzNhLWI4ZjYtNjFjZTEyYjJlNDVlL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1._SY209_CR0,0,140,209_.jpg");
+        expectedMovieList.add(movie2);
+
+        DefaultMovieService movieService = new DefaultMovieService(movieDao);
+
+        when(movieDao.getAll()).thenReturn(expectedMovieList);
+
+        List<Movie> actualMovieList = movieService.getAll();
+
+        assertEquals(2, actualMovieList.size());
+
+        for (int i = 0; i < actualMovieList.size(); i++) {
+            assertEquals(expectedMovieList.get(i).getId(), actualMovieList.get(i).getId());
+            assertEquals(expectedMovieList.get(i).getNameRussian(), actualMovieList.get(i).getNameRussian());
+            assertEquals(expectedMovieList.get(i).getNameNative(), actualMovieList.get(i).getNameNative());
+            assertEquals(expectedMovieList.get(i).getYearOfRelease(), actualMovieList.get(i).getYearOfRelease());
+            assertEquals(expectedMovieList.get(i).getRating(), actualMovieList.get(i).getRating(), 0.1);
+            assertEquals(expectedMovieList.get(i).getPrice(), actualMovieList.get(i).getPrice(), 0.1);
+            assertEquals(expectedMovieList.get(i).getPicturePath(), actualMovieList.get(i).getPicturePath());
+        }
+    }
+
+    public void getThreeRandomMovies() {
+
+        MovieDao movieDao = mock(MovieDao.class);
+
+        List<Movie> expectedMovieList = new ArrayList<>();
+
+        Movie movie1 = new Movie();
+        movie1.setId(1);
+        movie1.setNameRussian("Список Шиндлера");
+        movie1.setNameNative("Schindler's List");
+        movie1.setYearOfRelease("1993");
+        movie1.setRating(8.7);
+        movie1.setPrice(150.5);
+        movie1.setPicturePath("https://images-na.ssl-images-amazon.com/images/M/MV5BNDE4OTMxMTctNmRhYy00NWE2LTg3YzItYTk3M2UwOTU5Njg4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1._SX140_CR0,0,140,209_.jpg");
+        expectedMovieList.add(movie1);
+
+        Movie movie2 = new Movie();
+        movie2.setId(2);
+        movie2.setNameRussian("Пролетая над гнездом кукушки");
+        movie2.setNameNative("One Flew Over the Cuckoo''s Nest");
+        movie2.setYearOfRelease("1975");
+        movie2.setRating(8.7);
+        movie2.setPrice(180);
+        movie2.setPicturePath("https://images-na.ssl-images-amazon.com/images/M/MV5BZjA0OWVhOTAtYWQxNi00YzNhLWI4ZjYtNjFjZTEyYjJlNDVlL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1._SY209_CR0,0,140,209_.jpg");
+        expectedMovieList.add(movie2);
+
+        DefaultMovieService movieService = new DefaultMovieService(movieDao);
+
+        when(movieDao.getAll()).thenReturn(expectedMovieList);
+
+        List<Movie> actualMovieList = movieService.getAll();
+
+        assertEquals(2, actualMovieList.size());
+
+        for (int i = 0; i < actualMovieList.size(); i++) {
+            assertEquals(expectedMovieList.get(i).getId(), actualMovieList.get(i).getId());
+            assertEquals(expectedMovieList.get(i).getNameRussian(), actualMovieList.get(i).getNameRussian());
+            assertEquals(expectedMovieList.get(i).getNameNative(), actualMovieList.get(i).getNameNative());
+            assertEquals(expectedMovieList.get(i).getYearOfRelease(), actualMovieList.get(i).getYearOfRelease());
+            assertEquals(expectedMovieList.get(i).getRating(), actualMovieList.get(i).getRating(), 0.1);
+            assertEquals(expectedMovieList.get(i).getPrice(), actualMovieList.get(i).getPrice(), 0.1);
+            assertEquals(expectedMovieList.get(i).getPicturePath(), actualMovieList.get(i).getPicturePath());
+
+        }
+    }
+}
--- src/test/resources/db/create-db.sql
+++ src/test/resources/db/create-db.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS movie
+DROP TABLE movie IF EXISTS;
+CREATE TABLE  movie
 (
 	id  INTEGER IDENTITY PRIMARY KEY,
 	movie_name_rus VARCHAR(400),
 	movie_name_native VARCHAR(400),
-	release_date INTEGER ,
-	country VARCHAR(50),
+	release_date VARCHAR(400) ,
 	rating double precision default 0,
 	price double precision default 0,
 	poster varchar(4000)
